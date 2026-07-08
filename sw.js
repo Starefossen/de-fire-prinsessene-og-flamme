f09a0717bc883ad61c6e557c2b3498f7da3b85aa
Hans Kristian Flaatten <hans.kristian.flaatten@nav.no>
2026-07-08 13:01:02 +0200

    fix: reduce size of group image in prolog to allow Ellie to fit on the first page

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'prinsessene-v1783508305';
+const CACHE_NAME = 'prinsessene-v1783508462';
 const ASSETS = ["./","./index.html","./manifest.json","./bilder/persongalleri.jpg","./bilder/kap8-3.jpg","./bilder/kap8-2.jpg","./bilder/portrett-astrid.jpg","./bilder/kap8-1.jpg","./bilder/kap10-2.jpg","./bilder/kap10-3.jpg","./bilder/kap9-1.jpg","./bilder/kap9-3.jpg","./bilder/portett-hanna.jpg","./bilder/kap10-1.jpg","./bilder/portett-ellie.jpg","./bilder/kap9-2.jpg","./bilder/kap10-4.jpg","./bilder/kap10-5.jpg","./bilder/kap9-4.jpg","./bilder/kap3-2.jpg","./bilder/kap1-1.jpg","./bilder/kap3-3.jpg","./bilder/kap3-1.jpg","./bilder/kap1-3.jpg","./bilder/kap1-2.jpg","./bilder/kap7-4.jpg","./bilder/kap5-2.jpg","./bilder/portett-flamme.jpg","./bilder/kap5-3.jpg","./bilder/kap7-1.jpg","./bilder/kap7-3.jpg","./bilder/kap5-1.jpg","./bilder/portett-andre.jpg","./bilder/kap7-2.jpg","./bilder/kap2-1.jpg","./bilder/forside.jpg","./bilder/kap2-2.jpg","./bilder/kap2-3.jpg","./bilder/kap4-1.jpg","./bilder/portett-ela.jpg","./bilder/kap6-3.jpg","./bilder/kap6-2.jpg","./bilder/kap4-2.jpg","./bilder/kap6-1.jpg","./bilder/kap4-3.jpg"];
 
 self.addEventListener('install', event => {
